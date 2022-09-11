6138f29ea7d95af066461a6f481e6d9665045924
Juan José Bermúdez <juanjo@oficloud.com>
2022-09-11 22:58:05 +0200

    initial commit

--- src/components/Start.ts
+++ src/components/Start.ts
@@ -3,13 +3,20 @@ import { RenderableType } from './renderable'
 
 export class Start extends RenderableType {
     visibility:string
+    audio1: string
     constructor(id: string){
         super(id)
         this.visibility="visible"
+        this.audio1= "/sounds/117482-Casino_ambience_at_game_tables_1.mp3"
     }
     onStart(e:Event)
     {
+        e.preventDefault(); 
         this.visibility="hidden"
+        const audio = new Audio(this.audio1)
+        audio.loop=true
+        audio.volume=0.1
+        audio.play()
     }
     render(state: logic.GameState){
         //let listCards:string=""
